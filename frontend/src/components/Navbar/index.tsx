bcbd09ad66c0bc5e2c21944783cfef96bad4069a
Thiago Cabral <thiagounibratec@gmail.com>
2022-08-14 22:10:32 -0300

    Adicionado Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import{ReactComponent as GithubIcon} from '../../assets/img/github.svg';
+
+
+function Navbar(){
+
+    return (
+        <header>
+          <nav>
+          <div>
+          <h1>DSMovie</h1>
+          <a href="https://github.com/boodaabr">
+        <div>
+            <GithubIcon />
+           <p>/devsuperior</p>
+            </div>
+           </a>
+         </div>
+          </nav>
+        </header>
+      );
+
+}
+
+export default Navbar;
